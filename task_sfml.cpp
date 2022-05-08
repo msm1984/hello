d2fedda89168bbab4ca6f1ba1db9a40aacebf94f
mosadegh <sadeghmomo2020@gmail.com>
2022-05-09 00:26:15 +0430

    new game

--- task_sfml.cpp
+++ task_sfml.cpp
@@ -5,184 +5,113 @@
 #include <string>
 using namespace sf;
 using namespace std;
-void my_Rotate(RectangleShape &s, int k){
-    //Vector2f ori = s.getPosition();
-    //s.move(100,0);
-    //if(ori.y == 0 && ori.x != 600){
-        //s.move(100,0);
-        //s.setOrigin(100,0);
-    //}
-    s.rotate(-k);
-        //clock.restart();
-    /*if(ori.x == 600 && ori.y != 100){
-        s.setPosition()
-    }*/
-
-}
-void orig(RectangleShape &s){
-    Vector2f pos = s.getOrigin();
-    Vector2f pis = s.getPosition();
-    //s.setPosition(0,0);
-    if(pis.x != 500 && pis.x != 600 && pis.y == 0){
-    s.move(100,0);
-    if(pos.x == 100 && pos.y == 0)
-        s.setOrigin(100,100);
-    if(pos.x == 100 && pos.y == 100)
-        s.setOrigin(0,100);
-    if(pos.x == 0 && pos.y == 100)
-        s.setOrigin(0,0);
-    if(pos.x == 0 && pos.y == 0)
-        s.setOrigin(100,0);
+class Item {
+private:
+    RectangleShape shape;
+    Texture texture;
+public:
+    Item(int x, int y) {
+        shape.setSize(Vector2f(100, 100));
+        shape.setPosition(x, y);
+        //Texture texture;
+        texture.loadFromFile("orange.jpg");
+        shape.setTexture(&texture);
+        shape.setTextureRect(IntRect(10, 10, 270, 150));
+        //shape.setPosition(x,y);
     }
-    if(pis.x == 500 && pis.y == 0){
-        s.move(100,100);
-        s.setOrigin(0,100);
+    void movv(int x, int y) {
+        shape.move(x, y);
     }
-    if(pis.x == 600 && pis.y != 500){
-    s.move(0,100);
-    if(pos.x == 100 && pos.y == 0)
-        s.setOrigin(100,100);
-    if(pos.x == 100 && pos.y == 100)
-        s.setOrigin(0,100);
-    if(pos.x == 0 && pos.y == 100)
-        s.setOrigin(0,0);
-    if(pos.x == 0 && pos.y == 0)
-        s.setOrigin(100,0);
-    }
-    if(pis.y == 500 && pis.x == 600){
-        s.move(-100,100);
-        s.setOrigin(100,0);
-    }
-    if(pis.x != 100 && pis.y == 600){
-    s.move(-100,0);
-    if(pos.x == 100 && pos.y == 0)
-        s.setOrigin(100,100);
-    if(pos.x == 100 && pos.y == 100)
-        s.setOrigin(0,100);
-    if(pos.x == 0 && pos.y == 100)
-        s.setOrigin(0,0);
-    if(pos.x == 0 && pos.y == 0)
-        s.setOrigin(100,0);
-    }
-    if(pis.x == 100 && pis.y == 600){
-        s.move(-100,-100);
-        s.setOrigin(0,100);
-    }
-    if(pis.x == 0 && pis.y != 100){
-    s.move(0,-100);
-    if(pos.x == 100 && pos.y == 0)
-        s.setOrigin(100,100);
-    if(pos.x == 100 && pos.y == 100)
-        s.setOrigin(0,100);
-    if(pos.x == 0 && pos.y == 100)
-        s.setOrigin(0,0);
-    if(pos.x == 0 && pos.y == 0)
-        s.setOrigin(100,0);
-    }
-    if(pis.x == 0 && pis.y == 100){
-        s.move(100,-100);
-        s.setOrigin(100,0);
+    RectangleShape getShape() {
+        return shape;
     }
 
+};
+bool outOfScreen(Item& item) {
+    return item.getShape().getPosition().y == 650;
 }
+int score = 0;
+RectangleShape basket(Vector2f(100, 100));
+/*basket.setPosition(250,500);
+Texture texture;
+texture.loadFromFile("hello.jpg");
+basket.setTexture(&texture);
+basket.setTextureRect(IntRect(10, 10, 270, 150));*/
+bool collide(Item& item) {
+    if (item.getShape().getPosition().x == basket.getPosition().x &&
+        item.getShape().getPosition().y == basket.getPosition().y) {
+        score++;
+        return true;
+    }
+    return false;
+}
+
 
 int main()
 {
     RenderWindow window(VideoMode(600, 600), "SFML works!");
-    RectangleShape shape(Vector2f(100,100));
+    //RectangleShape basket(Vector2f(100,100));
+    basket.setPosition(250, 500);
     Texture texture;
-    texture.loadFromFile("wood.jpg");
-    shape.setTexture(&texture); // texture is a sf::Texture
-    shape.setTextureRect(sf::IntRect(10, 10, 100, 100));
+    texture.loadFromFile("hello.jpg");
+    basket.setTexture(&texture);
+    basket.setTextureRect(IntRect(10, 10, 270, 150));
+    Vector2f pos;
     Font font;
     font.loadFromFile("arial.ttf");
     Text text;
     text.setFont(font);
     text.setCharacterSize(24);
     text.setFillColor(Color::Red);
-    shape.move(100,0);
-    shape.setOrigin(100,0);
-    //sf::Vector2f origin = shape.getOrigin();
-    //shape.setRotation(-90);
-    //shape.rotate(5.f);
-    Clock clock;
-    Time elapse;
-    Clock clock1;
-    Time elapse1;
-    int k = 30;
     string r;
-    //text.setString(r);
-    text.setPosition(300,300);
-    //int i = 0;
-    srand(time(0));
-    int a = rand()%255;
-    int b = rand()%255;
-    int c = rand()%255;
-    int j = 0;
-    //a = rand()%200;
-    //shape.rotate(-10);
-
+    //Clock clock;
+    Clock clock;
+    Time time_1;
+    Time time_2;
+    vector<Item> items;
     while (window.isOpen())
     {
+
+        //Clock clock;
+        //Time time_1;
+        //Time time_2;
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            pos = basket.getPosition();
+            if (event.type == Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyPressed){
-                    if(event.key.code == Keyboard::A && k < 50){
-                            k += 10;
-                    }
-                    else if(event.key.code == Keyboard::D && k > 0){
-                            k -= 10;
-        }
-
+            if (event.type == Event::KeyReleased) {
+                if (event.key.code == Keyboard::A && pos.x > 0)
+                    basket.move(-50, 0);
+                if (event.key.code == Keyboard::D && pos.x < 500)
+                    basket.move(50, 0);
             }
-        }
 
-        if(j == 45){
-            j = 0;
+
+
         }
-        r = to_string(k);
+        time_2 = clock.getElapsedTime();
+        if (time_2.asMilliseconds() > 1000) {
+            clock.restart();
+            for (auto item = items.begin(); item != items.end(); item++) {
+                item->movv(0, 100);
+            }
+            items.erase(remove_if(items.begin(), items.end(), outOfScreen), items.end());
+            items.erase(remove_if(items.begin(), items.end(), collide), items.end());
+            items.push_back(Item({ (rand() % 11) * 100,0 }));
+        }
+        Vector2f pos;
+        r = to_string(score);
         text.setString(r);
-        elapse = clock.getElapsedTime();
-        if(elapse.asMilliseconds() > k){
-                my_Rotate(shape,2);
-                j++;
-        if(a != 255 && b != 255 && c != 255 && a != 0){
-            a++;
-        }
-        if(b != 255 && a == 255 && c != 255){
-            b++;
-        }
-        if(b == 255 && a == 255 && c != 255){
-            c++;
-        }
-        if(b == 255 && a != 0 && c == 255){
-            a--;
-        }
-        if(b != 0 && a == 0 && c == 255){
-            b--;
-        }
-        if(b == 0 && a == 0 && c != 0){
-            c--;
-        }
-        if(a == 0 && b == 0 & c == 0){
-            a++;
-        }
-        //my_Rotate(shape,2);
-                clock.restart();
-        }
-        window.clear(Color(c,b,a));
-        //my_Rotate(shape,2);
-        window.draw(shape);
+        window.clear(Color::White);
+        window.draw(basket);
+        for (Item item : items)
+            window.draw(item.getShape());
         window.draw(text);
         window.display();
-        //clock.restart();
-        if(j == 45){
-            orig(shape);
-        }
     }
 
     return 0;
+
 }
